c59aee57da160d999cbb24945a1d518f2f93db6d
apsinghrana100 <apsinghrana100@gmail.com>
2022-10-29 19:50:37 +0530

    getElementByClassId

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-console.log(document.domain);
-console.log(document.url);
-console.log(document.title);
-document.title="hello ajay";
+// console.log(document.domain);
+// console.log(document.url);
+// console.log(document.title);
+// document.title="hello ajay";
 
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.All);
-//console.log(document.All[10]);
-console.log(document.forms);
-console.log(document.links);
-console.log(document.images);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.All);
+// //console.log(document.All[10]);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
 
-const header_title=document.getElementById('main-header');
-header_title.style.borderBottom="solid 3px #000";
-const Main_item=document.getElementsByClassName('card card-body');
-console.log(Main_item);
-const main_item=document.getElementById('main');
-main_item.firstElementChild.innerHTML='<b>ADD ITEM</b>';
-main_item.firstElementChild.style.color="green";
+// const header_title=document.getElementById('main-header');
+// header_title.style.borderBottom="solid 3px #000";
+// const Main_item=document.getElementsByClassName('card card-body');
+// console.log(Main_item);
+// const main_item=document.getElementById('main');
+// main_item.firstElementChild.innerHTML='<b>ADD ITEM</b>';
+// main_item.firstElementChild.style.color="green";
 
-console.log("only for testing");
+
+//getelementbyclassid
+
+const items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='hello-2';
+items[1].style.fontWeight='Bold';
+items[1].style.backgroundColor='yellow';
+
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor="gray";
+}
+
+//Make the 3 rd element in the list have green background color
+
+items[2].style.backgroundColor="green";
+
+//Make all the elements in the list have bold color font
+
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight="bold";
+}
