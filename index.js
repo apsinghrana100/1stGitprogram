67cfc5525a8b577a0e94d3bd763da9cd29004c57
apsinghrana100 <apsinghrana100@gmail.com>
2022-10-29 21:35:15 +0530

    getElementBytagname

--- index.js
+++ index.js
@@ -21,23 +21,55 @@
 // main_item.firstElementChild.style.color="green";
 
 
-//getelementbyclassid
+// //getelementbyclassid
 
+ // items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='hello-2';
+// items[1].style.fontWeight='Bold';
+// items[1].style.backgroundColor='yellow';
+
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor="gray";
+// }
+
+// //Make the 3 rd element in the list have green background color
+
+// items[2].style.backgroundColor="green";
+
+// //Make all the elements in the list have bold color font
+
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight="bold";
+// }
+
+
+///And try editing it with getelementsbyclassname and then by getelementbytagname
 const items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='hello-2';
-items[1].style.fontWeight='Bold';
-items[1].style.backgroundColor='yellow';
+//items[5].style.fontWeight="bold";
+
+
+
+// //getelementbyclassid
+
+ li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello-2';
+li[1].style.fontWeight='Bold';
+li[1].style.backgroundColor='yellow';
 
 for(let i=0;i<items.length;i++)
 {
-    items[i].style.backgroundColor="gray";
+    li[i].style.backgroundColor="gray";
 }
 
 //Make the 3 rd element in the list have green background color
 
-items[2].style.backgroundColor="green";
+li[2].style.backgroundColor="green";
 
 //Make all the elements in the list have bold color font
 
@@ -45,3 +77,6 @@ for(let i=0;i<items.length;i++)
 {
     items[i].style.fontWeight="bold";
 }
+li[4].style.backgroundColor="Blue";
+
+
